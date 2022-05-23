cba622aedc92fa07ff21738847f6ef331377a3a3
jcalvin4 <joe.m.calvin@outlook.com>
2022-05-23 12:16:17 -0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+public class Main{
+    public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+        Class.forName("org.sqlite.JDBC");
+        c = DriverManager.getConnection("jdbc:sqlite:yugioh.db");
+        
+    }
+        catch(Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table Yugioh Connected Successfully");
+    }
+    
+}
